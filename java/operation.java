74724889335de327c9af455d092a2060391cd121
Namrata Singh <namsinghrajpoot9@gmail.com>
2025-09-01 14:26:26 +0530

    initial commit

---
+++ java/operation.java
@@ -0,0 +1,9 @@
+public class operation {
+    public static void main(String []args)
+    {
+       int x=6,y=10,z=15;
+       System.out.println("value of x is"+x +"  value of y is "+y+"  value of z is "+z);
+       System.out.println("hey"+x+y+z);
+       System.out.println("hey"+(x+y+z));
+    }
+}
---
+++ java/second.java
@@ -0,0 +1,13 @@
+public class second{
+    public static void main (String[] args)
+    {
+     int x=5;
+     System.out.println(x); 
+     x=7;
+     System.out.println(x); 
+     x=x+6;
+     System.out.println(x);
+     x=x-20;
+     System.out.println(x); 
+    }
+}
